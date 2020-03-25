bb3670f5682238f1d5755deae7f3e309f213f7b4
wildpow <element1028@gmail.com>
2020-03-24 18:03:04 -0700

    Updated hero text and posisition

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,21 +1,48 @@
 import React from "react"
-import { Heading, Box, Flex, Button } from "@chakra-ui/core"
-import tpHero from "../images/woman-in-mask-holding-toilet-paper-3962337.jpg"
+import { Heading, Box, Flex, Button, Text } from "@chakra-ui/core"
+import tpHero from "../images/woman-in-pink-long-sleeve-hoodie-carrying-tissue-rolls-3962433.jpg"
 const Hero = () => {
   return (
     <Box
       as="article"
-      bgImage={`url(${tpHero})`}
-      bgSize="cover"
+      // bgImage={`url(${tpHero})`}
       // bgPos="center"
       bgRepeat="no-repeat"
       h="90vh"
+      background={`linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    ), url(${tpHero})`}
+      bgSize="cover"
     >
-      <Flex flexDirection="column">
-        <Heading>TP Finder</Heading>
-        <p>Search local inventory of hundreds of stores in your area</p>
-        <Button>FIND TP NOW</Button>
-      </Flex>
+      <Box
+        maxW="32rem"
+        // flexDirection="column"
+        pos="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+      >
+        {/* <Heading as="h1" size="2xl">
+          Toilet Paper Finder
+        </Heading>
+        <p>
+          Stop using kleenex on your bum! Search the inventory of hundreds of
+          local stores in your area
+        </p>
+        <Button>FIND TP NOW</Button> */}
+        <Heading mb={4} as="h1" size="2xl">
+          Toilet Paper Finder
+        </Heading>
+        <Text fontSize="xl">
+          Using kleenex on your bum? Top living like a savege and search the
+          inventory of hundreds of local stores in your area to find toilet
+          paper today.
+        </Text>
+        <Button size="lg" variantColor="green" mt="24px">
+          Search Now
+        </Button>
+      </Box>
     </Box>
   )
 }
